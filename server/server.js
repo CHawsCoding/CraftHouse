7f7f4edfc498875c938683c4f4ccfb9b0cddda44
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-23 11:05:42 -0600

    create a new instance of a GraphQL server

--- server/server.js
+++ server/server.js
@@ -5,15 +5,45 @@ const path = require('path');
 //connect to the database
 const db = require('./config/connection');
 
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Move app.listen outside of the db.once callback
+//serve up static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  }); 
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      const user = authMiddleware({ req }).user;
+      return { user };
+    }
+  });
+
+  const startApolloServer = async () => {
+    await server.start();
+    server.applyMiddleware({ app });
+  };
+
+  startApolloServer();
+
 app.listen(PORT, () => {
     console.log(`🌍 Now listening on http://localhost:${PORT}`);
+    console.log(`🚀 GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`);
     });
 
 db.once('open', () => {
