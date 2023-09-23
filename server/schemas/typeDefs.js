dcbe4aa0092f5b89bc4d71771fe594d5669714c8
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-23 11:01:33 -0600

    type Defs defined in all type

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+type Query {
+    me: User
+    users: [User]
+    DIYs: [DIY]
+    DIY(_id: ID!): DIY
+    comment(_id: ID!): Comment
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addDIY(title: String!, description: String!, materialsUsed: [String], instructions: String!): DIY
+    addComment(content: String!): Comment
+    saveDIY(DIYId: ID!): User
+    removeDIY(DIYId: ID!): User
+    removeComment(commentId: ID!): DIY
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    DIYs: [DIY]
+    comments: [Comment]
+}
+
+type DIY {
+    _id: ID
+    title: String
+    description: String
+    materialsUsed: [String]
+    instructions: String
+    user: User
+    comments: [Comment]
+}
+
+type Comment {
+    _id: ID
+    content: String
+    user: User
+    DIY: DIY
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+`;
+
+module.exports = typeDefs;
