b28d9d468fe1878819d81f8918e244e51db2309f
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-25 18:00:37 -0600

    UI structure get updated for discussion

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -6,23 +6,33 @@ function Explore() {
   // Use the useQuery hook to fetch data
   const { loading, error, data } = useQuery(GET_ALL_DIYS);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error! {error.message}</div>;
+  if (loading) return <div className="text-center py-8">Loading...</div>;
+  if (error) return <div className="text-center py-8">Error! {error.message}</div>;
 
   const DIYs = data.DIYs; // Extract DIY data from the response
 
   return (
-    <div>
-      <h2>Explore DIYs</h2>
-      {DIYs.map((DIY) => (
-        <div key={DIY._id}>
-          <h3>{DIY.title}</h3>
-          <p>{DIY.description}</p>
-        </div>
-      ))}
+    <div className="container mx-auto py-8">
+      <h2 className="text-3xl font-semibold text-center mb-8">Explore DIYs</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {DIYs.map((DIY) => (
+          <div key={DIY._id} className="border rounded-lg shadow-md overflow-hidden">
+            {DIY.images && DIY.images.length > 0 && (
+              <img
+                src={DIY.images[0]} 
+                alt={DIY.title}
+                className="w-full h-48 object-cover"
+              />
+            )}
+            <div className="p-6">
+              <h3 className="text-xl font-semibold mb-2">{DIY.title}</h3>
+              <p className="text-gray-700">{DIY.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Explore;
-
