e91f574bb9eee42e517fb75c542a0c7e2a1752ec
Brayden Day <braydenday99@gmail.com>
2023-09-28 19:14:27 -0600

    edited schema and config to connect to mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
-
-mongoose.connect('mongodb://localhost:27017/gql', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-
 db.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
-
 db.once("open", () => {
   console.log("Connected to MongoDB!");
 });
-
 module.exports = db;
