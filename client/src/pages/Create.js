384dcd8b7eb40cd2f48faea2256b5777f7b8884f
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-30 14:41:09 -0600

    remove material field added to remove triggered input field

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -40,6 +40,16 @@ function Create() {
     });
   };
 
+  const removeMaterialField = (index) => {
+    const newMaterialsUsed = [...formState.materialsUsed];
+    newMaterialsUsed.splice(index, 1); // Remove the material input at the specified index
+
+    setFormState({
+      ...formState,
+      materialsUsed: newMaterialsUsed,
+    });
+  };
+
   const handleImageChange = (event) => {
     const selectedFiles = Array.from(event.target.files);
     const imageUrls = [];
@@ -98,10 +108,11 @@ function Create() {
   };
 
 
-return (
-  <div className="max-w-md mx-auto p-4">
-    <h2 className="text-2xl font-semibold mb-4">Create a New DIY</h2>
-    <form onSubmit={handleSubmit} encType="multipart/form-data">
+
+  return (
+    <div className="max-w-md mx-auto p-4">
+      <h2 className="text-2xl font-semibold mb-4">Create a New DIY</h2>
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
         <div className="mb-4">
           <label htmlFor="title" className="block text-gray-700 font-bold mb-2">
             Title:
@@ -143,14 +154,16 @@ return (
                 required
                 className="text-black w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
               />
-              {index === formState.materialsUsed.length - 1 && (
-                <button
+              <button
                   type="button"
-                  onClick={addMaterialField}
-                  className="bg-pink-500 hover:bg-pink-600 text-white rounded-full px-4 ml-2"
-                >
+                  onClick={addMaterialField} className="bg-pink-500 hover:bg-pink-600 text-white rounded-full px-4 ml-2">
                   +
                 </button>
+              {index === formState.materialsUsed.length - 1 && (
+                <button type="button" onClick={() => removeMaterialField(index)}
+                className="bg-pink-500 hover:bg-pink-600 text-white rounded-full px-4 ml-2">
+                -
+              </button>
               )}
             </div>
           ))}
@@ -165,8 +178,7 @@ return (
             value={formState.instructions}
             onChange={handleInputChange}
             required
-            className="text-black w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-          />
+            className="text-black w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:border-blue-500"/>
         </div>
         <div className="mb-4">
           <label htmlFor="images" className="block text-gray-700 font-bold mb-2">
