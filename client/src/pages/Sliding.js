2171adebe42e09cb20f6d168d4f446c7ac1c4784
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-29 20:38:01 -0600

    adding sliding pics

--- client/src/pages/Sliding.js
+++ client/src/pages/Sliding.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
+import sliding from '../images/sliding.jpg';
+import sliding2 from '../images/sliding2.jpg';
+import sliding3 from '../images/sliding3.jpg';
+import sliding4 from '../images/sliding4.jpg';
+import sliding5 from '../images/sliding5.jpg';
+import sliding6 from '../images/sliding6.jpg';
+
 const imageUrls = [
-  'https://media.gettyimages.com/id/117144601/photo/diy-disaster.jpg?s=612x612&w=0&k=20&c=48S4ySUO3_LIjeRmYRomLSQMAwGs9vZi7EddctODk_w=',
-  'https://media.gettyimages.com/id/1065109848/photo/wrapping-presents.jpg?s=612x612&w=0&k=20&c=r9mp-H8Fhrwc-mnB6DA4wSCf8lO1Wa2HeszkYQJSV6M=',
-  'https://media.gettyimages.com/id/1014773628/photo/african-jewellery-on-display-at-the-irene-market.jpg?s=612x612&w=0&k=20&c=Vl50Et3OKvmE5WoOEPy1PUeqcojMIbHfAk0GW-gyoZo=',
-  'https://media.gettyimages.com/id/171292804/photo/painted-hands-sign-heart.jpg?s=612x612&w=0&k=20&c=m3Pqlj7lxsSMVp68corQ8sGQwtnbMX1HnD98Ja9Beu4=',
-  'https://images.pexels.com/photos/5760780/pexels-photo-5760780.jpeg?auto=compress&cs=tinysrgb&w=400',
-  'https://media.gettyimages.com/id/1255615584/photo/woman-is-coloring-a-chair-at-home.jpg?s=612x612&w=0&k=20&c=xcJ2UPVRWmcD3ot8MV2uUsOVtGKwRgaylTi2R0uDTkc=',
+  sliding,
+  sliding2,
+  sliding3,
+  sliding4,
+  sliding5,
+  sliding6,
 ];
 
 function Sliding() {
