72c3f638e73e81163feda1b1b2ff94b0ffc1ed58
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-09-30 22:11:31 -0600

    savedDIYs and DIYs in user profile handle delete and removed from saved functions

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,53 +1,75 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
 import { GET_ME } from '../utils/queries';
+import { REMOVE_DIY } from '../utils/mutations';
 
 import profile from '../images/profile.png';
-import explore from '../images/explore.jpg';
 import SavedDIY from './savedDIY';
 
 function Profile() {
   const { loading, error, data } = useQuery(GET_ME);
+  const [deleteDIYMutation] = useMutation(REMOVE_DIY);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error! {error.message}</div>;
 
   const user = data.me;
 
+  const handleDeleteDIY = async (diyId) => {
+    try {
+      await deleteDIYMutation({
+        variables: { DIYId: diyId },
+        refetchQueries: [{ query: GET_ME }], // Refetch the user data after deletion
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="explore-container">
-    <div className="flex flex-col items-center p-8">
-      {/* Profile Picture */}
-      <div className="rounded-full w-20">
-        
-        <img src= { profile } alt="Profile" className="object-cover w-full h-full" />
-      </div>
-      <h2 className="text-2xl font-semibold mt-4">My Profile</h2>
-      <p className="text-gray-500">Username: {user.username}</p>
-      <p className="text-gray-500">Email: {user.email}</p>
+    <div className="min-h-screen p-8">
+      <div className="max-w-4xl mx-auto bg-gray-900 rounded-lg p-8">
+        <div className="flex flex-col items-center">
+          <div className="rounded-full w-32 h-32 overflow-hidden">
+            <img src={profile} alt="Profile" className="object-cover w-full h-full" />
+          </div>
+          <h2 className="text-2xl font-semibold mt-4">My Profile</h2>
+          <p className="text-gray-500">Username: {user.username}</p>
+          <p className="text-gray-500">Email: {user.email}</p>
+        </div>
 
-
-      <div className="container mx-auto p-4">
-        <h3 className="text-3xl font-semibold text-yellow-500 text-center">My DIYs</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mt-4">
-
-          {user.DIYs.map((diy) => (
-            <div key={diy._id} className="border rounded-lg shadow-md overflow-hidden">
-              {/* DIY content */}
-              <div className="p-4">
-                <h4 className=" underline text-lg font-semibold">{diy.title}</h4>
+        <div className="mt-8">
+          <div className="border-b border-t m-5 p-2 border-gray-500">
+            <h3 className="text-3xl font-semibold text-yellow-500 text-center">My DIYs</h3>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-4">
+            {user.DIYs.map((diy) => (
+              <div key={diy._id} className="bg-bl border rounded-lg shadow-md overflow-hidden">
+                <img src={diy.images[0]} alt="my DIY" className="object-cover min-w-full" />
+                <div className="p-4">
+                  <h4 className="text-lg font-semibold">{diy.title}</h4>
+                </div>
+                <div className="flex justify-between p-4 border-t border-gray-300">
+             {/* View DIY */}
+                  <Link to={`/diy/${diy._id}`} className="block p-4 hover:bg-gray-900">
+                    <h3 className="text-lg text-green-500 hover:text-green-600 font-semibold mb-2 text-center underline">View</h3>
+                  </Link>
+                  <button
+                    className="text-red-500 hover:text-red-600"
+                    onClick={() => handleDeleteDIY(diy._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className="p-4 border-t border-gray-400">
-                <div className="text-sm text-gray-400"><img src = {diy.images[0]} alt="my DIYs" className="object-cover w-1/2 text-center" /></div>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-
-    </div>
-    <h3 className="font-semibold mt-4 text-center text-3xl text-yellow-500 border-t border-gray-500 underline ">My favourites</h3>
-     <SavedDIY />
+      <div className="mt-8">
+        <SavedDIY />
+      </div>
     </div>
   );
 }
--- client/src/pages/savedDIY.js
+++ client/src/pages/savedDIY.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
 import { GET_SAVED_DIYS } from '../utils/queries';
+import { REMOVE_SAVED_DIY } from '../utils/mutations';
 
 function SavedDIY({ userId }) {
   const { loading, error, data } = useQuery(GET_SAVED_DIYS, {
     variables: { userId },
   });
+  const [removeSavedDIYMutation] = useMutation(REMOVE_SAVED_DIY);
 
   if (loading) return <div className="text-center py-8">Loading...</div>;
   if (error) return <div className="text-center py-8">Error! {error.message}</div>;
 
   const savedDIYs = data.getSavedDIYs;
 
+  const handleRemoveSavedDIY = async (diyId) => {
+    try {
+      await removeSavedDIYMutation({
+        variables: { DIYId: diyId },
+        refetchQueries: [{ query: GET_SAVED_DIYS, variables: { userId } }], // Refetch the saved DIYs after removal
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="container mx-auto p-4">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-6 mt-4 ml-4 mr-4">
+    <div className="container mx-auto px-4 py-8">
+        <div className="border-b border-t m-5 p-2 border-gray-500">
+          <h3 className="text-3xl font-semibold text-yellow-500 text-center">My Saved DIYs</h3>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-6 mt-4 ml-4 mr-4">
       {savedDIYs.map((diy) => (
         <div
           key={diy._id}
@@ -22,10 +39,27 @@ function SavedDIY({ userId }) {
         >
           {/* DIY content */}
           <div className="p-4">
-            <h4 className="text-yellow-500 text-lg font-semibold text-center">{diy.title}</h4>
+            <h4 className="text-gray-500 text-lg font-semibold text-center">{diy.title}</h4>
           </div>
-          <div className="p-4 border-t border-gray-400">
-            <p className="text-sm text-gray-200">Description: {diy.description}</p>
+          {/* DIY image */}
+          <div className="flex justify-center border-t">
+            <img
+              src={diy.images[0]}
+              alt="DIY"
+              className="object-cover w-full"
+            />
+          </div>
+          {/* View DIY */}
+          <Link to={`/diy/${diy._id}`} className="block p-4 hover:bg-gray-900">
+            <h3 className="text-lg text-yellow-500 hover:text-yellow-600 font-semibold mb-2 text-center underline">View</h3>
+          </Link>
+          <div className="flex justify-between p-4 border-t border-gray-300">
+            <button
+              className="text-red-500 hover:text-red-600"
+              onClick={() => handleRemoveSavedDIY(diy._id)}
+            >
+              Remove from saved
+            </button>
           </div>
         </div>
       ))}
@@ -35,3 +69,4 @@ function SavedDIY({ userId }) {
 }
 
 export default SavedDIY;
+            
