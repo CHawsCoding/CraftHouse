ac49a46e1b31c7d3f00069f2ed5dc297487e1a43
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-10-01 18:32:27 -0600

    home page title and texts styles changed

--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -41,29 +41,31 @@ const imageUrls = [
 function Hero() {
   return (
     <div className="container mx-auto p-4">
+      <section className="first">
       <div className="flex flex-wrap-reverse items-center">
         {/* Big Picture on the Right */}
         <div className="w-full md:w-1/2 mb-4 md:mb-0">
           <img
             src={landing}
             alt="Big Image"
-            className="w-full h-auto"
+            className="w-full h-auto object-cover"
           />
         </div>
 
         {/* Text Content on the Left */}
-        <div className="w-full md:w-1/2 md:pr-8">
-          <h1 className="text-2xl font-bold mb-4 ml-10">Welcome to CraftHouse!</h1>
-          <p className="mb-4 ml-20">
+        <div className="w-full md:w-1/2 md:pl-10">
+          <h1 className="lg:text-7xl md:text-5xl sm:text-3xl text-yellow-500 text-center font-bold">Welcome to CraftHouse!</h1>
+          <p className="lg:text-3xl text-center md:text-2xl">
             Join the Community and Start sharing your creative DIY ideas today!
           </p>
-          <p className="mb-4 ml-20">
+          <p className="text-yellow-500 text-center lg:text-2xl">
            Interact with other DIYers and spread the inspiration!
           </p>
           {/* Add more text content as needed */}
         </div>
       </div>
-
+      </section>
+      <section className="second">
       {/* Image Grid */}
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
         {imageUrls.map((imageUrl, index) => (
@@ -78,11 +80,12 @@ function Hero() {
         ))}
       </div>
 
+      </section>      
       <section className="popular-diy-section bg-pink-100 py-12 mt-6">
         <div className="container mx-auto">
           <h2 className="text-3xl text-black font-semibold text-center mb-6">Popular DIYs</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {/* Add popular DIYs here */}
+            {/* letsAdd popular DIYs here */}
           </div>
         </div>
       </section>
