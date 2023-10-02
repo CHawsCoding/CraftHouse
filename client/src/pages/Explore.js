a776aae62a580eff3866c10a4251841de31f4692
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-10-01 22:54:10 -0600

    card frames size fixed

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -4,10 +4,12 @@ import { GET_ALL_DIYS} from '../utils/queries';
 import { Link } from 'react-router-dom';
 import { ADD_COMMENT, ADD_LIKE, REMOVE_LIKE, SAVE_DIY } from '../utils/mutations';
 
+//import icons for users interaction section
 import { SlLike } from 'react-icons/sl';
 import { SlDislike } from 'react-icons/sl';
 import { HiOutlineSaveAs } from 'react-icons/hi';
 
+//import components that serve fetching likes and comments from the database
 import Likes from '../components/Likes';
 import Comments from '../components/Comments';
 
@@ -17,24 +19,28 @@ function Explore({ primaryColor }) {
   const [comments, setComments] = useState({});
   // const [savedDIYs, setSavedDIYs] = useState([]);
 
+  //mutations for likes, comments, and savedDIYs
   const [addLikeMutation] = useMutation(ADD_LIKE);
   const [addCommentMutation] = useMutation(ADD_COMMENT);
   const [removeLikeMutation] = useMutation(REMOVE_LIKE);
   const [saveDIYMutation] = useMutation(SAVE_DIY);
 
+  //it will return a loading message if the data is still loading, an error message if there is an error, or the DIYs if the query is successful
   if (loading) return <div className="text-center py-8">Loading...</div>;
   if (error) return <div className="text-center py-8">Error! {error.message}</div>;
 
+  //DIYs in the database
   const DIYs = data && data.DIYs ? data.DIYs : [];
 
-
+// a function to handle likes
   const handleLike = async (id) => {
     try {
-      const { data } = await addLikeMutation({
-        variables: { DIYId: id },
-        refetchQueries: [{ query: GET_ALL_DIYS }],
+      const { data } = await addLikeMutation({ // mutation to add a like
+        variables: { DIYId: id }, // pass the DIY's id to the mutation
+        refetchQueries: [{ query: GET_ALL_DIYS }],// refetch the GET_ALL_DIYS query to update the cache
       });
 
+      // update the likes state variable with the DIY's updated likes
       const updatedLikes = data.addLike.likes;
       setLikes((prevLikes) => ({
         ...prevLikes,
@@ -45,6 +51,7 @@ function Explore({ primaryColor }) {
     }
   };
 
+  // a function to handle dislikes
   const handleDislike = async (id) => {
     try {
       const { data } = await removeLikeMutation({
@@ -62,12 +69,13 @@ function Explore({ primaryColor }) {
     }
   };
 
+  // a function to handle comments
   const handleComment = async (e, id) => {
     e.preventDefault(); // Prevent the default form submission
-    const commentInput = e.target.commentInput.value;
+    const commentInput = e.target.commentInput.value; // Get the comment input value
     try {
-      const { data } = await addCommentMutation({
-        variables: { DIYId: id, content: commentInput },
+      const { data } = await addCommentMutation({ // Mutation to add a comment
+        variables: { DIYId: id, content: commentInput }, // Pass the DIY's id and the comment content to the mutation
       });
 
       const updatedComments = data.addComment.comments;
@@ -75,13 +83,13 @@ function Explore({ primaryColor }) {
         ...prevComments,
         [id]: updatedComments,
       }));
-      e.target.commentInput.value = '';
+      e.target.commentInput.value = ''; // Clear the comment input field
     } catch (error) {
       console.error(error);
     }
   };
 
-  
+  // a function to handle saving DIYs
   const handleSave = async (id) => {
     try {
       const { data } = await saveDIYMutation({
@@ -100,23 +108,25 @@ function Explore({ primaryColor }) {
         <h2 className="text-3xl font-semibold text-center mb-8 text-yellow-600">Explore DIYs</h2>
         <div className="grid grid-cols-1 mr-5 ml-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {DIYs.map((DIY, index) => (
-            <div key={DIY._id} className="bg-gray-900 border-2 border-gray-800 rounded-lg shadow-lg transition transform duration-300 hover:scale-105 overflow-hidden">
+            <div key={DIY._id} className="bg-gray-900 border-2 border-gray-800 rounded-lg shadow-lg transition transform duration-300 hover:scale-105 overflow-hidden" style={{ width: '100%', height: '100%' }}>
               {DIY.images && DIY.images.length > 0 && (
                 <img
                   src={DIY.images[0]}
                   alt={DIY.title}
-                  className="w-1/2 object-fit mx-auto"/>
+                  className="w-full h-40 object-cover" // Set the image to full width and specify the height (adjust as needed)
+                />
               )}
-              <div className="p-6 border-t">              
-                  <h3 className="text-xl text-gray-500 font-semibold mb-2 text-center underline">{DIY.title}</h3>
+              <div className="p-6 border-t">
+                <h3 className="text-xl text-gray-500 font-semibold mb-2 text-center underline">{DIY.title}</h3>
               </div>
-              <div className="text-center p-4">               
-                  <p className="text-gray-300">{DIY.description}</p>
+              <div className="text-center p-4">
+                <p className="text-gray-300">{DIY.description}</p>
                 <Link to={`/diy/${DIY._id}`} className="block p-4 hover:bg-gray-900">
                   <h3 className="text-lg text-yellow-500 hover:text-yellow-600 font-semibold mb-2 text-center underline">Learn More</h3>
                 </Link>
               </div>
-              <div className='flex flex-auto border-b border-gray-800'>
+           {/* likes section rendered from likes.js*/}
+           <div className='flex flex-auto border-b border-gray-800'>
                 <Likes DIYId={DIY._id} />
               </div>
 
@@ -146,7 +156,7 @@ function Explore({ primaryColor }) {
                     }
                   />
                 </form>
-                <Comments DIYId={DIY._id} />
+                <Comments DIYId={DIY._id} /> {/* comments rendered from comments.js*/}
               </div>
               
             </div>
