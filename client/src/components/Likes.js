0552a09735f62ad090eb40d3a0cc524b2c580eda
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2023-10-02 23:02:33 -0600

    worked on the auto-refresh issues - still not working for likes but we did resolve some of the browser dev tools websocket errors

--- client/src/components/Likes.js
+++ client/src/components/Likes.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_LIKED_USERS } from '../utils/queries';
-import { NEW_LIKE_SUBSCRIPTION } from '../utils/subscriptions';
-import { useSubscription } from '@apollo/client'; // Import useSubscription
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_LIKED_USERS } from "../utils/queries";
+import { NEW_LIKE_SUBSCRIPTION } from "../utils/subscriptions";
+import { useSubscription } from "@apollo/client"; // Import useSubscription
 
 function Likes({ DIYId }) {
   const { loading, error, data, refetch } = useQuery(GET_LIKED_USERS, {
     variables: { DIYId },
   });
+
+  const [localLikedUsers, setLocalLikedUsers] = useState([]);
+
+  useEffect(() => {
+    if (data && data.getLikedUsers) {
+      setLocalLikedUsers(data.getLikedUsers);
+    }
+  }, [data]);
 
   // Subscribe to new likes
   const { data: newLikeData } = useSubscription(NEW_LIKE_SUBSCRIPTION, {
@@ -15,7 +23,8 @@ function Likes({ DIYId }) {
   });
 
   if (loading) return <div className="text-center py-8">Loading...</div>;
-  if (error) return <div className="text-center py-8">Error! {error.message}</div>;
+  if (error)
+    return <div className="text-center py-8">Error! {error.message}</div>;
 
   const likedUsers = data.getLikedUsers;
 
@@ -24,19 +33,19 @@ function Likes({ DIYId }) {
   // Check if there's new like data from the subscription
   if (newLikeData && newLikeData.newLike) {
     // Add the new like user to the likedUsers array
-    likedUsers.push(newLikeData.newLike.user);
+    setLocalLikedUsers((prevUsers) => [...prevUsers, newLikeData.newLike.user]);
     refetch(); // Refetch the query to update the cache
   }
 
   const uniqueUsernames = new Set();
 
-  likedUsers.forEach((likedUser) => {
+  localLikedUsers.forEach((likedUser) => {
     uniqueUsernames.add(likedUser.username);
   });
 
   const uniqueUsernamesArray = Array.from(uniqueUsernames);
 
-  const totalLikes = likedUsers.length;
+  const totalLikes = localLikedUsers.length;
 
   return (
     <div className="ml-2">
@@ -45,9 +54,16 @@ function Likes({ DIYId }) {
       ) : (
         <>
           {uniqueUsernamesArray.length === 1 ? (
-            <p className="text-gray-600"> {uniqueUsernamesArray[0]} likes this</p>
+            <p className="text-gray-600">
+              {" "}
+              {uniqueUsernamesArray[0]} likes this
+            </p>
           ) : (
-            <p className="text-gray-600"> {uniqueUsernamesArray[0]} and {uniqueUsernamesArray.length - 1} others liked this</p>
+            <p className="text-gray-600">
+              {" "}
+              {uniqueUsernamesArray[0]} and {uniqueUsernamesArray.length - 1}{" "}
+              others liked this
+            </p>
           )}
         </>
       )}
--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ALL_DIYS, GET_LIKED_USERS } from '../utils/queries';
-import { Link } from 'react-router-dom';
-import { ADD_COMMENT, ADD_LIKE, REMOVE_LIKE, SAVE_DIY } from '../utils/mutations';
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_ALL_DIYS, GET_LIKED_USERS } from "../utils/queries";
+import { Link } from "react-router-dom";
+import {
+  ADD_COMMENT,
+  ADD_LIKE,
+  REMOVE_LIKE,
+  SAVE_DIY,
+} from "../utils/mutations";
 
 // Import icons for user interaction section
-import { SlLike } from 'react-icons/sl';
-import { SlDislike } from 'react-icons/sl';
-import { HiOutlineSaveAs } from 'react-icons/hi';
+import { SlLike } from "react-icons/sl";
+import { SlDislike } from "react-icons/sl";
+import { HiOutlineSaveAs } from "react-icons/hi";
 
 // Import components that serve fetching likes and comments from the database
-import Likes from '../components/Likes';
-import Comments from '../components/Comments';
+import Likes from "../components/Likes";
+import Comments from "../components/Comments";
 
 function Explore({ primaryColor }) {
   const { loading, error, data } = useQuery(GET_ALL_DIYS);
@@ -26,14 +31,14 @@ function Explore({ primaryColor }) {
   const [removeLikeMutation] = useMutation(REMOVE_LIKE);
   const [saveDIYMutation] = useMutation(SAVE_DIY);
 
-
   if (loading) return <div className="text-center py-8">Loading...</div>;
-  if (error) return <div className="text-center py-8">Error! {error.message}</div>;
+  if (error)
+    return <div className="text-center py-8">Error! {error.message}</div>;
 
   //DIYs in the database
   const DIYs = data && data.DIYs ? data.DIYs : [];
 
- // a function to handle likes
+  // a function to handle likes
   const handleLike = async (id) => {
     try {
       await addLikeMutation({
@@ -77,7 +82,8 @@ function Explore({ primaryColor }) {
     e.preventDefault(); // Prevent the default form submission
     const commentInput = e.target.commentInput.value; // Get the comment input value
     try {
-      const { data } = await addCommentMutation({ // Mutation to add a comment
+      const { data } = await addCommentMutation({
+        // Mutation to add a comment
         variables: { DIYId: id, content: commentInput }, // Pass the DIY's id and the comment content to the mutation
       });
 
@@ -86,7 +92,7 @@ function Explore({ primaryColor }) {
         ...prevComments,
         [id]: updatedComments,
       }));
-      e.target.commentInput.value = ''; // Clear the comment input field
+      e.target.commentInput.value = ""; // Clear the comment input field
     } catch (error) {
       console.error(error);
     }
@@ -99,71 +105,100 @@ function Explore({ primaryColor }) {
         variables: { DIYId: id },
       });
       console.log(data);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
-    <div style={{ backgroundColor: primaryColor, color: 'white', padding: '16px' }} className="explore-container">
+    <div
+      style={{ backgroundColor: primaryColor, color: "white", padding: "16px" }}
+      className="explore-container"
+    >
       <div className="container mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-8 text-yellow-600">Explore DIYs</h2>
+        <h2 className="text-3xl font-semibold text-center mb-8 text-yellow-600">
+          Explore DIYs
+        </h2>
         <div className="grid grid-cols-1 mr-5 ml-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {DIYs.map((DIY, index) => (
-            <div key={DIY._id} className="bg-gray-900 border-2 border-gray-800 rounded-lg shadow-lg transition transform duration-300 hover:scale-105 overflow-hidden" style={{ width: '100%', height: '100%' }}>
-              {DIY.images && DIY.images.length > 0 && (
-                <img
-                  src={DIY.images[0]}
-                  alt={DIY.title}
-                  className="w-full h-40 object-cover"
-                />
-              )}
-              <div className="p-6 border-t">
-                <h3 className="text-xl text-gray-500 font-semibold mb-2 text-center underline">{DIY.title}</h3>
-              </div>
-              <div className="text-center p-4">
-                <p className="text-gray-300">{DIY.description}</p>
-                <Link to={`/diy/${DIY._id}`} className="block p-4 hover:bg-gray-900">
-                  <h3 className="text-lg text-yellow-500 hover:text-yellow-600 font-semibold mb-2 text-center underline">Learn More</h3>
-                </Link>
-              </div>
-           {/* likes section rendered from likes.js*/}
-           <div className='flex flex-auto border-b border-gray-800'>
-                <Likes DIYId={DIY._id} />
-              </div>
+          {DIYs &&
+            DIYs.map((DIY, index) => (
+              <div
+                key={DIY._id}
+                className="bg-gray-900 border-2 border-gray-800 rounded-lg shadow-lg transition transform duration-300 hover:scale-105 overflow-hidden"
+                style={{ width: "100%", height: "100%" }}
+              >
+                {DIY.images && DIY.images.length > 0 && (
+                  <img
+                    src={DIY.images[0]}
+                    alt={DIY.title}
+                    className="w-full h-40 object-cover"
+                  />
+                )}
+                <div className="p-6 border-t">
+                  <h3 className="text-xl text-gray-500 font-semibold mb-2 text-center underline">
+                    {DIY.title}
+                  </h3>
+                </div>
+                <div className="text-center p-4">
+                  <p className="text-gray-300">{DIY.description}</p>
+                  <Link
+                    to={`/diy/${DIY._id}`}
+                    className="block p-4 hover:bg-gray-900"
+                  >
+                    <h3 className="text-lg text-yellow-500 hover:text-yellow-600 font-semibold mb-2 text-center underline">
+                      Learn More
+                    </h3>
+                  </Link>
+                </div>
+                {/* likes section rendered from likes.js*/}
+                <div className="flex flex-auto border-b border-gray-800">
+                  <Likes DIYId={DIY._id} />
+                </div>
 
-              {/* users interaction section */}
-              <div className="flex justify-between px-6 py-4">
-                <SlLike size={24} className="text-white-600 hover:text-yellow-500 hover:scale-125 cursor-pointer" onClick={() => handleLike(DIY._id)}/>
-                <SlDislike size={24} className="text-white-600 hover:text-red-500 hover:scale-125 cursor-pointer" onClick={() => handleDislike(DIY._id)}/>
-                <HiOutlineSaveAs size={24} className="text-white-600 hover:text-yellow-500 hover:scale-125 cursor-pointer" onClick={() => handleSave(DIY._id)}/>
-              </div>
+                {/* users interaction section */}
+                <div className="flex justify-between px-6 py-4">
+                  <SlLike
+                    size={24}
+                    className="text-white-600 hover:text-yellow-500 hover:scale-125 cursor-pointer"
+                    onClick={() => handleLike(DIY._id)}
+                  />
+                  <SlDislike
+                    size={24}
+                    className="text-white-600 hover:text-red-500 hover:scale-125 cursor-pointer"
+                    onClick={() => handleDislike(DIY._id)}
+                  />
+                  <HiOutlineSaveAs
+                    size={24}
+                    className="text-white-600 hover:text-yellow-500 hover:scale-125 cursor-pointer"
+                    onClick={() => handleSave(DIY._id)}
+                  />
+                </div>
 
-              {/* Comment section */}
-              <div className="relative">
-                <form className="commentForm" onSubmit={(e) => handleComment(e, DIY._id)}>
-                  <input
-                    type="text"
-                    placeholder="Add a comment..."
-                    className="text-yellow-50 p-2 w-full bg-slate-600"
-                    name="commentInput"
-                    onClick={
-                      () => {
-                        if (localStorage.getItem('id_token')) {
+                {/* Comment section */}
+                <div className="relative">
+                  <form
+                    className="commentForm"
+                    onSubmit={(e) => handleComment(e, DIY._id)}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Add a comment..."
+                      className="text-yellow-50 p-2 w-full bg-slate-600"
+                      name="commentInput"
+                      onClick={() => {
+                        if (localStorage.getItem("id_token")) {
                           return;
                         } else {
-                          alert('Please log in to comment');
+                          alert("Please log in to comment");
                         }
-                      }
-                    }
-                  />
-                </form>
-                <Comments DIYId={DIY._id} /> {/* comments rendered from comments.js*/}
+                      }}
+                    />
+                  </form>
+                  <Comments DIYId={DIY._id} />{" "}
+                  {/* comments rendered from comments.js*/}
+                </div>
               </div>
-              
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
