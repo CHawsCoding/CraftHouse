d11c6596b468610df039ca7dc24f91d791db03d0
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-10-02 18:25:26 -0600

    added an event listener to the document in the useEffect hook to capture clicks outside of the modal.

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_COMMENTS } from '../utils/queries';
 
@@ -13,6 +13,23 @@ function Comments({ DIYId }) {
 
   // State variables to track displayed comments and whether the modal is open
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    if (isModalOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isModalOpen]);
 
   if (loading) return <div className="text-center py-8">Loading comments...</div>;
   if (error) return <div className="text-center py-8">Error! {error.message}</div>;
@@ -33,10 +50,10 @@ function Comments({ DIYId }) {
     <div className="mt-4">
       {comments.length > 0 && (
         <div className="mb-2 ml-2">
-          {/* user name and comment icons section */}
+           {/* user name and comment icons section */}
           <div className='flex justify-between'>
-          <p className="font-semibold text-gray-600 text-xs">{comments[0].user.username}</p>
-          <GiNothingToSay size={18} className='text-gray-700'/> 
+            <p className="font-semibold text-gray-600 text-xs">{comments[0].user.username} :</p>
+            <GiNothingToSay size={18} className='text-gray-700'/>
           </div>
           <p className="text-gray-500 bg-slate-800 p-4 rounded ">{comments[0].content}</p>
         </div>
@@ -47,11 +64,11 @@ function Comments({ DIYId }) {
           View All Comments
         </button>
       )}
-
-      {/* Comments Modal */}
+      
+        {/* Comments Modal */}
       {isModalOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-opacity-50 bg-black z-50 overflow-y-auto">
-          <div className="bg-white p-4 border-white rounded-lg shadow-lg w-11/12 max-w-xl border"> 
+          <div ref={modalRef} className="bg-white p-4 border-white rounded-lg shadow-lg w-11/12 max-w-xl border"> 
             {comments.map((comment, index) => (
               <div key={index} className="mb-2">
                 <p className="font-semibold text-indigo-700">{comment.user.username}</p>
